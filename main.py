07858891f118f97ce5f36a5e94c1b102e0da6606
Alessandro Schaer <aschaer@magnes.ch>
2020-09-16 18:28:49 +0200

    Basic selection and templates

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ import WarBot
 def main():
     """Run the full game."""
 
+    print(__doc__)
     world_file = WorldSelector.select_world()
     wb = WarBot.WarBot(world_file)
     wb.run()
+
+
 
 if __name__ == "__main__":
     main()
--- src/WorldSelector.py
+++ src/WorldSelector.py
@@ -14,16 +14,57 @@ DEFAULT_WORLD = "Debugland"
 
 
 def select_world(root: str = ".") -> str:
-    """Select world from a list."""
+    """Select world from a list.
 
-    res = "/".join((root, WORLDS_FOLDER, DEFAULT_WORLD, DEFAULT_FILENAME))
+    Selection can be done by index (the one in the printed list) or name of the
+    detected worlds. Worlds need to be in the worlds/ folder and feature a
+    states.json file.
 
-    return res
+    """
+
+    default = "/".join((root, WORLDS_FOLDER, DEFAULT_WORLD, DEFAULT_FILENAME))
+
+    pname, dnames, fnames = next(os.walk("/".join((root, WORLDS_FOLDER))))
+    print("World-selection. Plese select a world, choose from: ")
+    for ii, dn in enumerate(dnames):
+        try:
+            print("\t{}. {}".format(ii + 1, get_last_part_of_path(dn)))
+
+        except:
+            pass
+
+
+    choice = input("\nSelect by name or number (int) [Default: 1. {}]: ".format(
+        get_last_part_of_path(dnames[0])
+        )
+    )
+    print()
+
+    try:
+        idx = int(choice) - 1
+        sel = dnames[idx] if idx >= 0 and idx < len(dnames) else DEFAULT_WORLD
+        selected = "/".join((root, WORLDS_FOLDER, sel, DEFAULT_FILENAME))
+
+    except:
+        selected = "/".join((root, WORLDS_FOLDER, choice, DEFAULT_FILENAME))
+
+    if os.path.exists(selected) and os.path.isfile(selected):
+        return selected
+
+    else:
+        return default
+
+
+
+def get_last_part_of_path(path: str) -> str:
+    """Return the last part of a path string (subdir or file) as a string."""
+
+    return path.split("/")[-1]
 
 
 
 if __name__ == "__main__":
     print(__doc__)
 
-    file_path = select_world()
+    file_path = select_world(".")
     print(file_path)
