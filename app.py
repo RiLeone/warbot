a4dfd2aa5f41e204f507b41adc06a44f6979f8e1
Filippo Martinoni <filippo.martinoni@gmail.com>
2020-09-28 00:15:02 +0200

    first sketch

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, redirect, url_for, render_template, request
+from flask import render_template
+
+from io import StringIO
+import sys
+
+### Install requirements.txt
+### Run it from warbot folder with 'flask run'
+### Look at the webpage http://127.0.0.1:5000/
+
+sys.path.append("src/")
+
+import WarBot
+import WorldSelector
+import AuxiliaryTools
+import HistoricStatistician as hs
+
+def main():
+    """Run the full game."""
+
+    print(__doc__)
+    options = AuxiliaryTools.parse_args()
+
+    world_file = './worlds/Debugland/states.json'
+    wb = WarBot.WarBot(world_file)
+    wb.run(**options["WarBot.run"])
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    text = "Hello! welcome to warbot"
+    bold_text = "Whup Whup!"
+    return render_template('home.html', text=text, bold_text=bold_text)
+
+
+
+@app.route("/warbot_text", methods=["POST", "GET"])
+def warbot_text():
+    if request.method == "POST":
+        world = request.form["world"]
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = StringIO()
+        main()
+        sys.stdout = old_stdout
+        return render_template('warbot_game.html', textt=mystdout.getvalue())
+    else:
+        textt = "no_text"
+        return render_template('warbot_game.html')
+
+
+@app.route("/warbot_svg")
+def warbot_svg():
+    text = "Hello! welcome to warbot"
+    bold_text = "Whup Whup!"
+    return render_template('home.html', text=text, bold_text=bold_text)
